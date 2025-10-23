28ca6b4e71162bafeed4ffacfe62e36b5f05632e
0xkoiner <alexgray.bcn.esp@gmail.com>
2025-10-23 18:53:38 +0200

    (test) EOASessionKeyCustodial

---
+++ test/benchmarks/3rd-Party-SessionKey/BenchmarksEOASessionKeyCustodialEmpty.t.sol
@@ -0,0 +1,168 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.29;
+
+import { VmSafe } from "lib/forge-std/src/Vm.sol";
+import { DeployAccount } from "test/DeployAccount.t.sol";
+import { IKeysManager } from "src/interfaces/IKeysManager.sol";
+import { console2 as console } from "lib/forge-std/src/Test.sol";
+import { PackedUserOperation } from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+
+contract BenchmarksEOASessionKeyCustodialEmpty is DeployAccount {
+    address internal reciver;
+    PubKey internal pK;
+
+    function setUp() public override {
+        super.setUp();
+        reciver = makeAddr("reciver");
+        _createQuickFreshKey(true);
+        _createCustomFreshKey(
+            false, KeyType.EOA, uint48(block.timestamp + 1 days), 0, 10, _getKeyEOA(sessionKey), KeyControl.Custodial
+        );
+        _initializeAccount();
+        _mint(owner7702, 3000e18);
+        _approveAll(address(erc20), owner7702, type(uint256).max, address(pm));
+        _warmUpAccount();
+    }
+
+    function test_SendEmptyCallWithEOASessionKeyCustodialDirectAA() external {
+        Call[] memory calls = _getCalls(1, reciver, 0 ether, hex"");
+
+        PackedUserOperation memory userOp = _getFreshUserOp(owner7702);
+        userOp = _populateUserOp(
+            userOp,
+            _packCallData(mode_1, calls),
+            _packAccountGasLimits(600_000, 400_000),
+            800_000,
+            _packGasFees(80 gwei, 15 gwei),
+            hex""
+        );
+
+        bytes memory signature = _signUserOpWithSK(userOp);
+        userOp.signature = _encodeEOASignature(signature);
+
+        _relayUserOp(userOp, "test_SendEmptyCallWithEOASessionKeyCustodialDirectAA");
+    }
+
+    function test_SendEmptyCallWithEOASessionKeyCustodialAASponsored() external {
+        Call[] memory calls = _getCalls(1, reciver, 0 ether, hex"");
+
+        PackedUserOperation memory userOp = _getFreshUserOp(owner7702);
+        bytes32 accountGasLimits = _packAccountGasLimits(600_000, 400_000);
+        userOp.accountGasLimits = accountGasLimits;
+        bytes memory paymasterAndData = _createPaymasterDataMode(userOp, VERIFYING_MODE, 0);
+        userOp = _populateUserOp(
+            userOp,
+            _packCallData(mode_1, calls),
+            accountGasLimits,
+            800_000,
+            _packGasFees(80 gwei, 15 gwei),
+            paymasterAndData
+        );
+
+        bytes memory paymasterSignature = this._signPaymasterData(VERIFYING_MODE, userOp, 1);
+        userOp.paymasterAndData = abi.encodePacked(userOp.paymasterAndData, paymasterSignature);
+
+        bytes memory signature = _signUserOpWithSK(userOp);
+        userOp.signature = _encodeEOASignature(signature);
+
+        _relayUserOp(userOp, "test_SendEmptyCallWithEOASessionKeyCustodialDirectAASponsored");
+    }
+
+    function test_SendEmptyCallWithEOASessionKeyCustodialAASponsoredERC20() external {
+        Call[] memory calls = _getCalls(1, reciver, 0 ether, hex"");
+
+        PackedUserOperation memory userOp = _getFreshUserOp(owner7702);
+        bytes32 accountGasLimits = _packAccountGasLimits(600_000, 400_000);
+        userOp.accountGasLimits = accountGasLimits;
+        bytes memory paymasterAndData = _createPaymasterDataMode(userOp, ERC20_MODE, combinedByteBasic);
+        userOp = _populateUserOp(
+            userOp,
+            _packCallData(mode_1, calls),
+            accountGasLimits,
+            800_000,
+            _packGasFees(80 gwei, 15 gwei),
+            paymasterAndData
+        );
+
+        bytes memory paymasterSignature = this._signPaymasterData(ERC20_MODE, userOp, 1);
+        userOp.paymasterAndData = abi.encodePacked(userOp.paymasterAndData, paymasterSignature);
+
+        bytes memory signature = _signUserOpWithSK(userOp);
+        userOp.signature = _encodeEOASignature(signature);
+
+        _relayUserOp(userOp, "test_SendEmptyCallWithEOASessionKeyCustodialDirectAASponsoredERC20");
+    }
+
+    function _relayUserOp(PackedUserOperation memory _userOp, string memory _testName) internal {
+        PackedUserOperation[] memory ops = new PackedUserOperation[](1);
+        ops[0] = _userOp;
+
+        _etch();
+        vm.prank(sender);
+        entryPoint.handleOps(ops, payable(sender));
+        if (bytes(_testName).length > 0) {
+            vm.snapshotGasLastCall(_testName);
+            VmSafe.Gas memory gas = vm.lastCallGas();
+        }
+    }
+
+    function _warmUpAccount()
+        internal
+        setTokenSpendM(KeyType.EOA, _getKeyEOA(sessionKey), NATIVE_ADDRESS, 30 ether, IKeysManager.SpendPeriod.Month)
+        setCanCallM(KeyType.EOA, _getKeyEOA(sessionKey), NATIVE_ADDRESS, EMPTY_CALLDATA_FN_SEL, true)
+        setCanCallM(KeyType.EOA, _getKeyEOA(sessionKey), reciver, EMPTY_CALLDATA_FN_SEL, true)
+    {
+        _depositToPM();
+
+        Call[] memory calls = _getCalls(1, reciver, 0.1 ether, hex"");
+
+        bytes memory executionData = abi.encode(calls);
+        _etch();
+        vm.prank(owner7702);
+        account.execute(mode_1, executionData);
+
+        bytes32 accountGasLimits = _packAccountGasLimits(600_000, 400_000);
+        bytes32 gasFees = _packGasFees(80 gwei, 15 gwei);
+        uint256 preVerificationGas = 800_000;
+
+        PackedUserOperation memory userOp = _getFreshUserOp(owner7702);
+        userOp =
+            _populateUserOp(userOp, _packCallData(mode_1, calls), accountGasLimits, preVerificationGas, gasFees, hex"");
+
+        bytes memory signature = _signUserOpWithSK(userOp);
+        userOp.signature = _encodeEOASignature(signature);
+
+        _relayUserOp(userOp, "");
+
+        userOp = _getFreshUserOp(owner7702);
+        userOp.accountGasLimits = accountGasLimits;
+        bytes memory paymasterAndData = _createPaymasterDataMode(userOp, VERIFYING_MODE, 0);
+        userOp = _populateUserOp(
+            userOp, _packCallData(mode_1, calls), accountGasLimits, preVerificationGas, gasFees, paymasterAndData
+        );
+
+        bytes memory paymasterSignature = this._signPaymasterData(VERIFYING_MODE, userOp, 1);
+        userOp.paymasterAndData = abi.encodePacked(userOp.paymasterAndData, paymasterSignature);
+
+        signature = _signUserOpWithSK(userOp);
+        userOp.signature = _encodeEOASignature(signature);
+
+        _relayUserOp(userOp, "");
+
+        userOp = _getFreshUserOp(owner7702);
+        userOp.accountGasLimits = accountGasLimits;
+        paymasterAndData = _createPaymasterDataMode(userOp, ERC20_MODE, combinedByteBasic);
+        userOp = _populateUserOp(
+            userOp, _packCallData(mode_1, calls), accountGasLimits, preVerificationGas, gasFees, paymasterAndData
+        );
+
+        paymasterSignature = this._signPaymasterData(ERC20_MODE, userOp, 1);
+        userOp.paymasterAndData = abi.encodePacked(userOp.paymasterAndData, paymasterSignature);
+
+        signature = _signUserOpWithSK(userOp);
+        userOp.signature = _encodeEOASignature(signature);
+
+        _relayUserOp(userOp, "");
+    }
+}
